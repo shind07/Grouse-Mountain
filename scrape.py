504e23ef00f910effba7f62eba1a699788e2fbd7
shind07 <shind@sfu.ca>
2019-01-12 07:47:59 -0800

    Get forecasted conditions for today and tomorrow

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 import lxml.html
 import requests
 
+def parse_text(text):
+    return list(filter(lambda x: len(x) > 0, text.split('\n')))
 def main():
     # url = 'https://www.grousemountain.com/current_conditions'
     # response = requests.get(url)
@@ -10,16 +12,20 @@ def main():
     conditions = tree.xpath('//div[@class="current-weather__content"]')[0].text_content()
     # celsius = temps.find('C')
     # print(temps[celsius-2])
-    parsed_conditions = list(filter(lambda x: len(x) > 0, conditions.split('\n')))
+    parsed_conditions = parse_text(conditions)
     celsius = parsed_conditions[0]
     weather = parsed_conditions[-1]
 
     current_status = tree.xpath('//div[@class="current_status"]')[0].text_content()
-    parsed_status = list(filter(lambda x: len(x) > 0, current_status.split('\n')))
+    parsed_status = parse_text(current_status)
     status = '{} {}'.format(*parsed_status[:2])
     report_type = '{} {}'.format(*parsed_status[2:4])
 
-    row = [celsius, weather, status, report_type]
+
+    forecast = tree.xpath('//div[@class="forecast"]')[0].text_content()
+    parsed_forecast = parse_text(forecast)
+    today, tomorrow = parsed_forecast[1], parsed_forecast[3]
+    row = [celsius, weather, status, report_type, today, tomorrow]
     print(row)
 
 
